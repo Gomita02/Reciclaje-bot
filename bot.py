401ac0023481334f10ed617018e0d63b951944ac
Gomita02 <j.gom1203@gmail.com>
2026-09-17 20:14:42 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+from residuos import residuos
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+
+@bot.command()
+async def reciclar(ctx, *, objeto):
+
+    objeto = objeto.lower()
+
+    if objeto in residuos:
+
+        informacion = residuos[objeto]
+
+        await ctx.send(
+            f"{informacion['emoji']} **{objeto.title()}**\n"
+            f"Categoría: **{informacion['categoria'].title()}**\n"
+            f"💡 {informacion['mensaje']}"
+        )
+
+    else:
+
+        await ctx.send(
+            f"❓ No conozco el residuo **{objeto}**.\n"
+            "Puedes intentar con otro objeto."
+        )
+
+
+bot.run("MTU1MDMwOTYwMDUwOTU1ODc4NA.GoVQ1h.B58Cm54E9E_FlAgAIXY2lrd50H8dZhyk7spCxc")
